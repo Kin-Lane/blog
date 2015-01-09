4c967becea40ba315e64993284c4fd19c229a019
Kin Lane <info@apievangelist.com>
2015-01-09 13:06:57 -0800

    loop through each api

--- js/editor-apisjson.js
+++ js/editor-apisjson.js
@@ -4,14 +4,36 @@ function loadJSONEditor()
 	
 	$APIsJSON_Path = "/blogapi/apis.json"
 	
-	console.log($APIsJSON_Path);
+	console.log("Pulling APIs.json: " + $APIsJSON_Path);
 	
     $.getJSON($APIsJSON_Path, function($apisjson) {    	  
     	  
     	console.log($apisjson);  
-
-    	$("#debugme").val($apisjson);
     	
+    	$APIsJSON_Name = $apisjson['name'];
+    	$APIsJSON_Desc = $apisjson['description'];
+    	$APIsJSON_Image = $apisjson['image'];
+    	$APIsJSON_Tags = $apisjson['tags'];
+    	
+    	$APIsJSON_APIs = $apisjson['apis'];
+    	$APIsJSON_Includes = $apisjson['include'];
+    	$APIsJSON_Maintainers = $apisjson['maintainers'];
+
+		console.log("Lets loop through each API...");  
+		
+         $.each($APIsJSON_APIs, function(apiKey, apiVal) {         	   
+           
+            $API_Name = apiVal['name'];
+            console.log($API_Name);  
+            $API_Desc = apiVal['description'];
+            $API_Image = apiVal['image'];   
+            $API_HumanURL = apiVal['humanURL'];   
+            $API_BaseURL = apiVal['baseURL'];   
+            $API_Tags = apiVal['tags'];   
+            $API_Properties = apiVal['properties'];   
+            $API_Contact = apiVal['contact'];       
+            
+             });	          	
                                                
         });
         	
