a3b63b21b536ae989bab427af7d34ea79f261079
Kin Lane <info@apievangelist.com>
2015-01-09 19:32:46 -0800

    debug

--- js/editor-apisjson.js
+++ js/editor-apisjson.js
@@ -14,11 +14,11 @@ function showme($row)
 	console.log('3:' + $thisrow);
 	if($thisrow=='none')
 		{
-		document.getElementById($thisslug).style.display; = '';	
+		document.getElementById($thisslug).style.display = '';	
 		}
 	else
 		{
-		document.getElementById($thisslug).style.display; = 'none';	
+		document.getElementById($thisslug).style.display = 'none';	
 		}			
 	}	
 
