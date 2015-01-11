1fdcc73a69d61587ab8baf68e8949b1144bce1a7
Kin Lane <info@apievangelist.com>
2015-01-10 23:18:47 -0800

    debug

--- js/editor-apis-json.js
+++ js/editor-apis-json.js
@@ -64,6 +64,12 @@ function APISJSONSave()
         
 	var repo = github.getRepo('Stack-Network','blogapi');  	
 
+	    repo.write('gh-pages', 'apis.json', $WriteAPIsJSON, 'Saving...', function(err) {
+	
+	        console.log("Doing Write!!");
+	        
+	    	});	
+
 	}
 
 // Header
