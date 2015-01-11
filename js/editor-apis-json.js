37aba294a428a68bb4cdbe6e4274ebc8421cd16b
Kin Lane <info@apievangelist.com>
2015-01-10 17:04:02 -0800

    debug

--- js/editor-apis-json.js
+++ js/editor-apis-json.js
@@ -358,10 +358,10 @@ function SaveAPIProperty($apicount,$propertycount)
  	$MasterAPISJSON['apis'][$apicount]['properties'][$propertycount]['url'] = $propertyUrl;
 
 	//$html = getPropertyListingCell1($propertyType,$propertyURL,$apicount,$propertycount); 			
-	document.getElementById('api-' + $apicount + '-property-' + $propertycount + '-1').innerHTML = "";
+	document.getElementById('api-' + $apicount + '-property-' + $propertycount + '-1').innerHTML = "hello";
 	
 	//$html = getPropertyListingCell2($propertyType,$propertyURL,$apicount,$propertycount); 			
-	document.getElementById('api-' + $apicount + '-property-' + $propertycount + '-2').innerHTML = "";				
+	document.getElementById('api-' + $apicount + '-property-' + $propertycount + '-2').innerHTML = "there";				
 	}	
 	
 function getPropertyEditListing($apiName,$thistype,$thisurl,$apicount,$propertycount)
