bddfc27e634369182ddc1033bb092cb9fc05366e
Kin Lane <info@apievangelist.com>
2015-01-11 23:12:16 -0800

    debug

--- js/editor-swagger-json.js
+++ js/editor-swagger-json.js
@@ -1014,16 +1014,30 @@ function SwaggerGetDefinitionsTitle()
 	}		
 	
 function SwaggerGetDefinitions($definition,$definitioncount)
-	{		
-	html = '<tr style="background-color:#CCC;">';
-	html = html + '<td colspan="2" style="padding-top: 5px; padding-bottom: 5px;">';
+	{				
+	
+	html = '<tr>';
+	html = html + '<td colspan="2" style="padding-top: 5px; padding-bottom: 5px;" align="center">';
+	
+
+	html = html + '<table border="0" width="80%" align="center" style="background-color:#CCC;">';
+	
+    html = html + '<tr>';
+    html = html + '<td>';	
+	
 	html = html + '<span style="font-size:20px;">';
-	html = html + '<strong>' + $definition + '</strong>';    
-	html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="add-definition-' + $definitioncount + '-property" title="Edit Swagger Definitions"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="20" align="right"  /></a>';							
+	html = html + '<strong>' + $definition + '</strong>';
 	html = html + '</span>';
+	
+	html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="add-definition-' + $definitioncount + '-property" title="Edit Swagger Header"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>';			
+	
+    html = html + '</td>';
+    html = html + '</tr>';		
+	
+	html = html + '</table>';
+	
 	html = html + '</td>';
 	html = html + '</tr>';
-
 	return html; 			
 	}		
 	
