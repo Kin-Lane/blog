76df16a18d29edb0be0825478ee84c9109dc3a12
Kin Lane <info@apievangelist.com>
2015-01-11 20:23:48 -0800

    debug

--- js/editor-swagger-json.js
+++ js/editor-swagger-json.js
@@ -379,7 +379,7 @@ function SwaggerSavePathVerbDetail($pathcount,$pathverbcount)
 	//$MasterSwagger['paths'][$pathcount][$pathverbcount]['operationId'] = $path_verb_operationid;
 	
 	console.log("Path Count: " + $pathcount);
-	$Temp = JSON.stringify($MasterSwagger['paths']);
+	$Temp = JSON.stringify($MasterSwagger['paths'][$pathcount]);
 	console.log("Temp: " + $Temp);
 	// Need a Rebuild
 
