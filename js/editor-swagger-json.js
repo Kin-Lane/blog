fd5ee80b9c795e83e0c18dbca1fe8ead1726fc02
Kin Lane <info@apievangelist.com>
2015-01-11 18:55:11 -0800

    debug

--- js/editor-swagger-json.js
+++ js/editor-swagger-json.js
@@ -443,8 +443,8 @@ function SwaggerGetPathVerbParameter($parameter_name,$parameter_in,$parameter_de
     
     html = html + '<tr>';
     html = html + '<td align="right" width="25%" style="font-size: 12px;"></td>';
-    html = html + '<td align="center" id="swagger-header-swagger-version-view" style="font-size: 12px;">';
-    html = html + $parameter_name + ' (' + $parameter_in + ')' + ' (' + $parameter_type + ') - ' + $parameter_desc;
+    html = html + '<td align="left" id="swagger-header-swagger-version-view" style="font-size: 12px;">';
+    html = html + '<strong>' + $parameter_name + '</strong> (' + $parameter_in + ')' + ' (' + $parameter_type + ') - ' + $parameter_desc;
     html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="edit-path-' + $pathcount + '-verb-' + $pathverbcount + '-property-' + $pathverbpropertycount + '-icon" title="Edit Swagger Header"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-edit-circle.png" width="20" align="right"  /></a>';			 
     html = html + '</td>';
     html = html + '</tr>';              
