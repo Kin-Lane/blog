34c8af8495f94dd99b5600fc2072f8bfa708162e
Kin Lane <info@apievangelist.com>
2015-01-12 13:09:47 -0800

    debug

--- js/editor-swagger-json.js
+++ js/editor-swagger-json.js
@@ -91,10 +91,7 @@ function SwaggerSave()
 			$path = treeValue['path'];
 			$sha = treeValue['sha'];
 			console.log($path);
-			if($path=='definitions/swagger.json')
-				{							
-			    repo.writemanual('gh-pages', 'definitions/swagger.json', $WriteAPIsJSON, 'Save', $sha, function(err) { });									
-				}
+		
 			});
 		});  	    	
 	}
