86c1868daf440c47da607809cb2b418d46edac16
Kin Lane <info@apievangelist.com>
2015-01-11 22:59:16 -0800

    debug

--- js/editor-swagger-json.js
+++ js/editor-swagger-json.js
@@ -1002,52 +1002,28 @@ function SwaggerGetEditPathVerbTag($tag,$pathcount,$pathverbcount,$pathverbtagco
 // Definitions
 function SwaggerGetDefinitionsTitle()
 	{
-	html = '<tr>';
-	html = html + '<td colspan="2" style="padding-top: 5px; padding-bottom: 5px;" align="center">';	
-
-	html = html + '<table border="0" width="70%" align="center" style="background-color:#CCC;">';
-	
-    html = html + '<tr>';
-    html = html + '<td>';	
-	
-	html = html + '<span style="font-size:16px;">';
-	html = html + '<strong>Definitions</strong>';
+	html = '<tr style="background-color:#CCC;">';
+	html = html + '<td colspan="2" style="padding-top: 5px; padding-bottom: 5px;">';
+	html = html + '<span style="font-size:20px;">';
+	html = html + '<strong>' + $pathTitle + '</strong>';
+	html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="add-definition-icon" title="Add a Path"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>';
 	html = html + '</span>';
-	
-	html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="add-definition-icon" title="Edit Swagger Tag"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>';			
-	
-    html = html + '</td>';
-    html = html + '</tr>';		
-	
-	html = html + '</table>';
-	
 	html = html + '</td>';
 	html = html + '</tr>';
-	return html; 			
+	return html;  					
 	}		
 	
 function SwaggerGetDefinitions($definition,$definitioncount)
 	{		
-    html = '<tr>';
-    html = html + '<td align="center" valign="top" colspan="2" id="apisjsonHeaderCell">';
-
-    html = html + '<table cellpadding="3" cellspacing="2" border="0" width="70%">';
-    
-    html = html + '<tr>';
-    html = html + '<td align="left" colspan="2" id="swagger-header-swagger-version-view" style="font-size: 12px;">';
-    html = html + '<strong>' + $definition + '</strong>';
-    
-    html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="add-property-icon" title="Add Property"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>';			
-	    			 
-    html = html + '</td>';
-    html = html + '</tr>';              
-
-    html = html + '</table>';
-
-    html = html + '</td>';
-    html = html + '</tr>';  
-
-	return html; 			
+	html = '<tr style="background-color:#CCC;">';
+	html = html + '<td colspan="2" style="padding-top: 5px; padding-bottom: 5px;">';
+	html = html + '<span style="font-size:20px;">';
+	html = html + '<strong>' + $definition + '</strong>';    
+	html = html + '<a href="#" onclick="SwaggerShowMe(this); return false;" id="add-definition-' + $definitioncount + '-property-icon" title="Edit Swagger Tag"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>';						
+	html = html + '</span>';
+	html = html + '</td>';
+	html = html + '</tr>';
+	return html; 					
 	}		
 	
 function SwaggerAddDefinition()
