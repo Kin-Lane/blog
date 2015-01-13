4be7ed90aa7283df81157af95fcb0aa5adaeea3b
Kin Lane <info@apievangelist.com>
2015-01-12 21:11:27 -0800

    debug

--- js/editor-blog-json.js
+++ js/editor-blog-json.js
@@ -12,7 +12,7 @@ function getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_
 	html = html + '<strong>' + $blog_name + '</strong>';
 	html = html + '</span>';
 
-    $html = $html + '<a href="#" onclick="BlogShowme(this); return false;" id="edit-blog-' + $blogcount + '-icon" title="Edit Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-edit-circle.png" width="35" align="right"  /></a>';
+    html = html + '<a href="#" onclick="BlogShowme(this); return false;" id="edit-blog-' + $blogcount + '-icon" title="Edit Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-edit-circle.png" width="35" align="right"  /></a>';
     //$html = $html + '<a href="#" onclick="BlogShowme(this); return false;" id="add-blog-' + $blogcount + '-icon" title="Add Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>'	
 	
 	html = html + '</td>';
