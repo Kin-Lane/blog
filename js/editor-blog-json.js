973d490834cc4ab437a92ffc2941957ca86c840f
Kin Lane <info@apievangelist.com>
2015-01-12 19:19:20 -0800

    debug

--- js/editor-blog-json.js
+++ js/editor-blog-json.js
@@ -29,6 +29,24 @@ function getBlogs()
 		});	
 		
 	}
+	
+function getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug,$blogcount)
+	{
+		
+	html = '<tr>';
+	html = html + '<td style="padding-top: 5px; padding-bottom: 5px;">';
+	html = html + '<span style="font-size:20px;">';
+	html = html + '<strong>' + $blog_name + '</strong>';
+	html = html + '</span>';
+
+    $html = $html + '<a href="#" onclick="BlogShowme(this); return false;" id="edit-blog-' + $blogcount + '-icon" title="Edit Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-edit-circle.png" width="35" align="right"  /></a>';
+    //$html = $html + '<a href="#" onclick="BlogShowme(this); return false;" id="add-blog-' + $blogcount + '-icon" title="Add Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-add-circle.png" width="35" align="right"  /></a>'	
+	
+	html = html + '</td>';
+	html = html + '</tr>';
+	return html; 
+				
+	}	
 
 function loadBlogEditor()
     {
@@ -36,9 +54,22 @@ function loadBlogEditor()
     console.log('loading blog editor');	
 
 	$response = getBlogs();
-
 	$responseJSON = JSON.stringify($response);	
-	console.log($responseJSON);
+	$blogcount = 0;
+	$.each($response, function(blogKey, blogValue) {
+		
+		$blog_name = blogValue['name'];
+		$blog_description = blogValue['description'];
+		$blog_url = blogValue['url'];
+		$blog_tags = blogValue['tags'];
+		$blog_slug = blogValue['slug'];
+		
+		$html = getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug);
+		$('#jsonBlogEditorTable').append($html); 
+		
+		$blogcount++;
+		});
+
 
 
 	}
