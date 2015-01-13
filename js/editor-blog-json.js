a7f93c45e334ac1ec8c73a04b0836a9cda91a47d
Kin Lane <info@apievangelist.com>
2015-01-13 13:04:57 -0800

    debug

--- js/editor-blog-json.js
+++ js/editor-blog-json.js
@@ -23,7 +23,7 @@ function getAddBlogPost()
     
     html = html + '<tr>';
     html = html + '<td align="right" style="background-color:#FFF;"><strong>description:</strong></td>';
-    html = html + '<td align="left" style="background-color:#FFF;"><textarea id="add-bog-url" name="Page_Body" rows="15" cols="50" class="mceEditor" style="width: 100%;"></td>';
+    html = html + '<td align="left" style="background-color:#FFF;"><textarea id="add-bog-description" name="Page_Body" rows="10" cols="50" class="mceEditor" style="width: 100%;"></td>';
     html = html + '</tr>';
     
     html = html + '<tr>';
