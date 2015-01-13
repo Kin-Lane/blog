0391fc677f6de6aa22beab4343d3136929b4a809
Kin Lane <info@apievangelist.com>
2015-01-12 17:00:01 -0800

    debug

--- js/editor-blog.js
+++ js/editor-blog.js
@@ -1,5 +1,23 @@
+
+//$.ajax({   url: 'path.php',   type: 'PUT',   success: function(data) {     //play with data }});
+
+
 function loadBlogEditor()
     {
     console.log('loading blog editor');	
+
+	$.ajax({
+		url: 'http://domain.api.stack.network/',   
+		type: 'GET',   
+		success: function(data) {
+		
+			$response = data;
+			$responseJSON = JSON.stringify(data);
+			
+			console.log($responseJSON);
+		
+			}
+		});
+
 
 	}
