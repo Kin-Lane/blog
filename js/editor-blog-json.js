6bdb97c04b01e5d332ef39c3d49f66eb06b132b6
Kin Lane <info@apievangelist.com>
2015-01-14 11:31:07 -0800

    debug

--- js/editor-blog-json.js
+++ js/editor-blog-json.js
@@ -110,7 +110,109 @@ function getAddBlogPost()
     html = html + '<br /></td></tr>';  
     	
 	return html; 			
-	}	
+	}
+	
+function addBlogPost($blogcount)
+	{
+	
+	$blog_name = document.getElementById("edit-blog-name-" + $blogcount).value;	
+	$blog_description = tinyMCE.get("edit-blog-description-" + $blogcount).getContent()
+	$blog_url = document.getElementById("edit-blog-url-" + $blogcount).value;
+	$blog_tags = document.getElementById("edit-blog-tags-" + $blogcount).value;
+	$blog_slug = document.getElementById("edit-blog-slug-" + $blogcount).value;	
+	
+	$postData = {};
+	  
+	$postData['appid'] = '5ed48098';
+	$postData['appkey'] = 'b6c8c8cba92815a6cdfe6e780bb0d2f5';
+
+	$postData['name'] = $blog_name;
+	$postData['description'] = $blog_description;
+	$postData['url'] = $blog_url;
+	$postData['tags'] = $blog_tags;
+	$postData['slug'] = $blog_slug
+		
+	$hosturl = 'http://blog.api.kinlane.com';
+	$baseurl = '/';
+	
+	$resource = 'blog/';
+
+	$apiurl = $hosturl + $baseurl + $resource;
+	
+	console.log($apiurl);
+	
+	$.ajax({
+		url: $apiurl,   
+		type: 'PUT', 
+		data: $postData,
+		success: function(data) {
+			
+			$WorkingResponse = data;
+				
+			$blogcount = 0;
+			
+			$.each(data, function(blogKey, blogValue) {
+				
+				$blog_name = blogValue['name'];
+				$blog_description = blogValue['description'];
+				$blog_url = blogValue['url'];
+				$blog_tags = blogValue['tags'];
+				$blog_slug = blogValue['slug'];
+				
+				//$html = getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug);
+				//$('#jsonBlogEditorTable').append($html); 
+				
+				});
+
+			
+			}
+		});		
+	
+	}		
+	
+function getEditBlogPost($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug,$blogcount)
+	{				
+		
+	html = '<tr id="add-blog-post" style="display: none;"><td align="center" style="font-size: 12px; background-color:#CCC; padding:5px;">';
+
+	html = html + '<span style="font-size: 18px;"><strong>Edit Blog</span></strong>';
+    html = html + '<table border="0" width="90%" id="edit-blog-post-table-' + $blogcount + '">';
+    
+    html = html + '<tr>';
+    html = html + '<td align="right" width="5%"><strong>name:</strong></td>';
+    html = html + '<td align="left"><input type="text" id="edit-blog-name" value="' + $blog_name + '" style="width:95%;" /></td>';
+    html = html + '</tr>';
+    
+    html = html + '<tr>';
+    html = html + '<td align="right"><strong>description:</strong></td>';
+    html = html + '<td align="left"><textarea id="edit-blog-description" name="Page_Body" rows="20" cols="50" class="mceEditor">' + $blog_description + '</textarea></td>';
+    html = html + '</tr>';
+    
+    html = html + '<tr>';
+    html = html + '<td align="right"><strong>url:</strong></td>';
+    html = html + '<td align="left"><input type="text" id="edit-blog-url" value="' + $blog_url + '" style="width:95%;" /></td>';
+    html = html + '</tr>';
+    
+    html = html + '<tr>';
+    html = html + '<td align="right"><strong>tags:</strong></td>';
+    html = html + '<td align="left"><input type="text" id="edit-blog-tags" value="' + $blog_tags + '" style="width:95%;" /></td>';
+    html = html + '</tr>'  
+    
+    html = html + '<tr>';
+    html = html + '<td align="right"><strong>slug:</strong></td>';
+    html = html + '<td align="left"><input type="text" id="edit-blog-slug" value="' + $blog_slug + '" style="width:95%;" /></td>';
+    html = html + '</tr>'      
+    
+    html = html + '<tr>';
+    html = html + '<td align="center" style="background-color:#FFF;" colspan="2"><input type="button" name="editAPIButton" value="Save" onclick="editBlogPost(' + $blogcount + ')" /></td>';
+    html = html + '</tr>'         
+     
+    html = html + '</table>';
+    
+    html = html + '<br /></td></tr>';  
+    	
+	return html; 			
+	}		
 	
 function getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug,$blogcount)
 	{
@@ -118,7 +220,7 @@ function getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_
 	html = '<tr>';
 	html = html + '<td style="padding-top: 5px; padding-bottom: 5px;">';	
 
-    html = html + '<a href="#" onclick="BlogShowme(this); return false;" id="edit-blog-' + $blogcount + '-icon" title="Edit Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-edit-circle.png" width="35" align="right"  /></a>';	
+    html = html + '<a href="#" onclick="BlogShowme(this); return false;" id="edit-blog-post-table-' + $blogcount + '-icon" title="Edit Blog Post"><img src="https://s3.amazonaws.com/kinlane-productions/bw-icons/bw-edit-circle.png" width="35" align="right"  /></a>';	
 	
 	html = html + '<span style="font-size:20px;">';
 	html = html + '<strong>' + $blog_name + '</strong>';
@@ -168,6 +270,9 @@ function loadBlogEditor()
 				$blog_slug = blogValue['slug'];
 				
 				$html = getBlogListing($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug);
+				$('#jsonBlogEditorTable').append($html); 
+				
+				$html = getEditBlogPost($blog_name,$blog_description,$blog_url,$blog_tags,$blog_slug,$blogcount)
 				$('#jsonBlogEditorTable').append($html); 
 				
 				$blogcount++;
