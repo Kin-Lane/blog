551767ef3bf4630de530f53305e88b3e87457b54
Kin Lane <info@apievangelist.com>
2015-01-15 22:14:14 -0800

    debug

--- js/editor-config-json.js
+++ js/editor-config-json.js
@@ -84,7 +84,7 @@ function getConfigGroup($config_group_name,$config_group_count)
 	
 function addNewConfig($configGroupKey,$config_group_coun)
 	{		
-	//console.log($configGroupKey);	
+	console.log($configGroupKey);	
 	}		
 	
 function getAddConfig($configGroupKey,$config_group_count)
