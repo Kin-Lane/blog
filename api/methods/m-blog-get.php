1c89e4300d673f65bb5072df3b80f0253c6577b2
Kin Lane <info@apievangelist.com>
2015-07-28 14:58:48 -0700

    add search field

--- api/methods/m-blog-get.php
+++ api/methods/m-blog-get.php
@@ -4,32 +4,32 @@ $app->get($route, function ()  use ($app){
 
 	$ReturnObject = array();
 
- 	$request = $app->request(); 
- 	$params = $request->params();	
+ 	$request = $app->request();
+ 	$params = $request->params();
 
 	if(isset($params['query'])){ $query = trim(mysql_real_escape_string($params['query'])); } else { $query = '';}
 	if(isset($params['page'])){ $page = trim(mysql_real_escape_string($params['page'])); } else { $page = 0;}
 	if(isset($params['count'])){ $count = trim(mysql_real_escape_string($params['count'])); } else { $count = 250;}
 	if(isset($params['sort'])){ $sort = trim(mysql_real_escape_string($params['sort'])); } else { $sort = 'Title';}
-	if(isset($params['order'])){ $order = trim(mysql_real_escape_string($params['order'])); } else { $order = 'DESC';}	
-			
+	if(isset($params['order'])){ $order = trim(mysql_real_escape_string($params['order'])); } else { $order = 'DESC';}
+
 	// Pull from MySQL
 	if($query!='')
 		{
-		$Query = "SELECT * FROM blog WHERE Title LIKE '%" . $query . "%'";
+		$Query = "SELECT * FROM blog WHERE Title LIKE '%" . $query . "%' OR Body LIKE '%" . $query . "%'";
 		}
-	else 
+	else
 		{
-		$Query = "SELECT * FROM blog";		
+		$Query = "SELECT * FROM blog";
 		}
 	$Query .= " ORDER BY " . $sort . " " . $order . " LIMIT " . $page . "," . $count;
 	//echo $Query . "<br />";
-	
+
 	$DatabaseResult = mysql_query($Query) or die('Query failed: ' . mysql_error());
-	  
+
 	while ($Database = mysql_fetch_assoc($DatabaseResult))
 		{
-			
+
 		$blog_id = $Database['ID'];
 		$post_date = $Database['Post_Date'];
 		$title = $Database['Title'];
@@ -43,18 +43,18 @@ $app->get($route, function ()  use ($app){
 		$twitterbuild = $Database['Twitter_Build'];
 		$linkedinbuild = $Database['LinkedIn_Build'];
 		$showonsite = $Database['Show_On_Site'];
-		$image = $Database['Feature_Image'];	
+		$image = $Database['Feature_Image'];
 
 		$TagQuery = "SELECT t.tag_id, t.tag from tags t";
 		$TagQuery .= " INNER JOIN blog_tag_pivot btp ON t.tag_id = btp.tag_id";
 		$TagQuery .= " WHERE btp.Blog_ID = " . $blog_id;
 		$TagQuery .= " ORDER BY t.tag DESC";
-		$TagResult = mysql_query($TagQuery) or die('Query failed: ' . mysql_error());		  
-				
+		$TagResult = mysql_query($TagQuery) or die('Query failed: ' . mysql_error());
+
 		// manipulation zone
 		$host = $_SERVER['HTTP_HOST'];
 		$blog_id = prepareIdOut($blog_id,$host);
-		
+
 		$F = array();
 		$F['blog_id'] = $blog_id;
 		$F['post_date'] = $post_date;
@@ -70,23 +70,23 @@ $app->get($route, function ()  use ($app){
 		$F['twitter_build'] = $twitterbuild;
 		$F['linkedin_build'] = $linkedinbuild;
 		$F['show_on_site'] = $showonsite;
-		
+
 		$F['tags'] = array();
 
 		while ($Tag = mysql_fetch_assoc($TagResult))
 			{
 			$thistag = $Tag['tag'];
-			
+
 			$T = array();
 			$T = $thistag;
 			array_push($F['tags'], $T);
-			//echo $thistag . "<br />";	
+			//echo $thistag . "<br />";
 			if($thistag=='Archive')
 				{
-				$archive = 1;	
-				}					
-			}		
-		
+				$archive = 1;
+				}
+			}
+
 		array_push($ReturnObject, $F);
 		}
 
